50d41c93c880e1973c59a8e433435d70f15530da
Chuck Meyer <chuck.meyer@algolia.com>
2021-11-12 18:40:23 -0500

    built action into dist

--- .github/actions/add-index-record/index.js
+++ .github/actions/add-index-record/index.js
@@ -4,6 +4,6 @@ const algoliasearch = require('algoliasearch');
 const client = algoliasearch(core.getInput('app_id'), core.getInput('api_key'));
 const index = client.initIndex(core.getInput('index_name'));
 
-index.saveObject(object).then(({ objectID }) => {
+index.saveObject(core.getInput('record')).then(({ objectID }) => {
   core.info(objectID);
 });
